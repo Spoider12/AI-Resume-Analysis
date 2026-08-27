329aeb0e76c569d135e805582f4bac4af6e5f953
Spoider12 <153114166+Spoider12@users.noreply.github.com>
2026-08-27 21:53:06 +0530

    CORS Error Fix

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,25 +1,21 @@
- const express = require('express');
- /* require all the routes here */
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
- const cookieParser = require("cookie-parser");
- const cors = require("cors")
+const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 
+const authRoutes = require("./routes/auth.route");
+const interviewRoutes = require("./routes/interview.route");
 
- const app = express();
+app.use("/api/auth", authRoutes);
+app.use("/api/interview", interviewRoutes);
 
- app.use(express.json());
- app.use(cookieParser());
- app.use(cors({
-    origin: "http://localhost:3000",
-    withCredentials: true
- }))
- /*require all the routes here*/
-  const authRoutes = require("./routes/auth.route");
-  const interviewRoutes = require("./routes/interview.route");
- /*using all the routes here*/
- app.use("/api/auth",authRoutes);
- app.use("/api/interview",interviewRoutes);
-
- module.exports = app;
+module.exports = app;
