b19e83cea58776141cdb84247b3fe3c1d6537b2a
Spoider12 <153114166+Spoider12@users.noreply.github.com>
2026-09-02 20:50:52 +0530

    provided fixed port

--- Backend/server.js
+++ Backend/server.js
@@ -9,6 +9,8 @@ const generateInterviewReport = require("./src/services/ai.service")
 connectDB();
 generateInterviewReport({resume, selfDescription,jobDescription})
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running on port ${port}`);
 });
